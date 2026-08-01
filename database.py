0532dcf5f9f83f4d2347d0f7edd5577e5229dfda
kavyaadivarapu08-source <kavyaadivarapu08@gmail.com>
2026-08-01 14:34:27 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,9 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/electronics_db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"ssl": {}}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
