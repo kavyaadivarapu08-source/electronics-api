87ac685be5942c2ab7f215ebe0c484261f8defc8
Kavyasri Adivarapu <kavyaadivarapu@gmail.com>
2026-08-06 16:15:13 +0530

    updated

--- crud.py
+++ crud.py
@@ -46,3 +46,54 @@ def delete_laptop(db: Session, laptop_id: int):
     db.commit()
 
     return db_laptop
+
+
+
+    
+
+def register_user(db, user: schemas.UserCreate):
+    existing_user = db.query(models.User).filter(
+        models.User.username == user.username
+    ).first()
+
+    if existing_user:
+        return None
+
+    hashed_password = hash_password(user.password)
+
+    new_user = models.User(
+        username=user.username,
+        email=user.email,
+        password=hashed_password,
+        role="user"
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
+def login_user(db, username: str, password: str):
+    user = db.query(models.User).filter(
+        models.User.username == username
+    ).first()
+
+    if not user:
+        return None
+
+    if not verify_password(password, user.password):
+        return None
+
+    token = create_access_token(
+        {
+            "sub": user.username,
+            "role": user.role
+        }
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
