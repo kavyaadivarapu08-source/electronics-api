9e843c277404264c496ec9ec972194c98a9443f5
Kavyasri Adivarapu <kavyaadivarapu@gmail.com>
2026-08-06 16:19:37 +0530

    updated

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from database import Base, engine, SessionLocal
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+app.include_router(auth.router)
 def get_db():
     db = SessionLocal()
     try:
