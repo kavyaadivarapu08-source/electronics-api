a346a6d71ff1b84f61591aec13a149e1e40563d0
Kavyasri Adivarapu <kavyaadivarapu@gmail.com>
2026-08-06 15:31:53 +0530

    test_update_delete

--- models.py
+++ models.py
@@ -60,3 +60,18 @@ class Headphone(Base):
     type = Column(String(30), nullable=False)
     connectivity = Column(String(30), nullable=False)
     price = Column(Integer, nullable=False)
+
+
+
+
+
+
+
+    class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    role = Column(String(20), default="user")
